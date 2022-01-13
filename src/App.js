fc417131cc4bf890b0d2f375f644af568578c33b
Max Ilalov <maximum_sill@mail.ru>
2022-01-13 14:16:41 +0300

    feat: init project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+export const App = () => {
+  return (
+    <div>
+      <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
